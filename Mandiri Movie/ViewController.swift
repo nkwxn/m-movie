68c8d3d2057276ece09706f721e18fd8315fd57e
Nicholas <nichz123@gmail.com>
2022-04-12 12:32:07 +0700

    Initial Commit

---
+++ Mandiri Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mandiri Movie
+//
+//  Created by Nicholas on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
